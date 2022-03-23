c19506be56d08631ea66e3f7f749ebb2b5f39e05
erakshaykumar <akaakshay304@gmail.com>
2022-03-23 22:22:42 +0530

    Added : Welcome Message to Mood Analyzer

---
+++ src/main/java/com/moodanalyzer/MoodAnalyzer.java
@@ -0,0 +1,12 @@
+/* @Purpose : Given a Welcome Message
+ * @File : Mood Analyzer
+ * @Author : Akshay Kumar
+ */
+package com.moodanalyzer;
+
+public class MoodAnalyzer {
+	public static void main(String[] args) {
+		System.out.println("Welcome To The Mood Analyzer Program");
+	}
+
+}
---
+++ src/test/java/com/moodanalyzer/MoodAnalyzerTest.java
@@ -0,0 +1,9 @@
+/*@Purpose : Give A Welcome Message 
+ *@File : Mood Analyzer Test
+ *@Author : Akshay Kumar
+ */
+package com.moodanalyzer;
+
+public class MoodAnalyzerTest {
+
+}
